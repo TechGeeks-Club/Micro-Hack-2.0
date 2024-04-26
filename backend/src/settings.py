0afdfade67c7a4d5b42776c41ee3b7a512dbfdfc
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-04-26 03:37:21 +0100

    CREATE : base user

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    
+    'task.apps.TaskConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
---
+++ backend/task/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Taks(models.Model):
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
