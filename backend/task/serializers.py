85b41b5bc2d1ae8d7c3c64509a0be4eeda393bbf
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-04-26 08:38:57 +0200

    fix

--- backend/task/serializers.py
+++ backend/task/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 from .models import Status,Priority,Taks,Comment
 
-class EventSerializers(serializers.ModelSerializer):
-    # ord_itm = ord_itm_srlz(many=True, read_only=True)
-    class Meta:
-        model            = Event
-        fields           = "__all__"
-        read_only_fields = ['created_by','created_at']
-        required_fields  = ['title','targets']
-
+ 
 
 class StatusSerializers(serializers.ModelSerializer):
     class Meta:
