4a38787c53fcba4aa91c5d513965484df6c8af7f
0x03bb <alibouakaz91@gmail.com>
2024-04-26 09:21:22 +0100

    add| auth

--- backend/src/urls.py
+++ backend/src/urls.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from django.urls import path,include
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/event/", include("event.urls")),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
+
+# auth endpoints:
+
