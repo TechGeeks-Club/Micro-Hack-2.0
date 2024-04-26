b5cc57f774da952155f7ae9503a0c9214fadf1f1
0x03bb <alibouakaz91@gmail.com>
2024-04-26 14:14:44 +0100

    add| get my events

--- backend/event/urls.py
+++ backend/event/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import ListEvents,GetEvents
+from .views import ListEvents,GetEvents,my_events
 
 urlpatterns = [
     path("", ListEvents.as_view()),
     path("<int:pk>", GetEvents.as_view()),
 
-    
+    path('a/',my_events),
 ]
--- backend/event/views.py
+++ backend/event/views.py
@@ -4,7 +4,24 @@ from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-from .models import Event 
+
+from django.http import HttpResponse, JsonResponse,response,HttpRequest
+# from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required, permission_required
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from django.shortcuts import get_object_or_404
+
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+
+
+from .models import Event
+
+from django.contrib.auth.models import Group 
 from .serializers import EventSerializers
 
 
@@ -21,3 +38,13 @@ class GetEvents(RetrieveAPIView):
     serializer_class = EventSerializers
     # permission_classes manage  = [IsAuthenticated] #! diasbled for testing
 
+
+@api_view(["GET"])
+def my_events(request):
+    user_group = request.user.groups.all()
+    #get group targets
+    Events = Event.objects.filter(targets__in=user_group)
+    sz = EventSerializers(Events,many=True)
+    if sz.is_valid:
+        return JsonResponse({'events':sz.data})
+    return JsonResponse("JsonResponse")
